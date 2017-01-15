46136ac86acf50d0f101750b091b7c1ea53682b0
xolodutra <pavelsahnov@yandex.ru>
2017-01-15 18:04:03 +0300

    закончил урок по bootstrap

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 my_flask_app = Flask(__name__)
 
 
 @my_flask_app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Свже нвсти от лчшх издтлств стрны", h1="Првт!")
+
+
+@my_flask_app.route("/all/")
+def all_news():
+    return render_template('all_news.html')
+
+
+@my_flask_app.route("/login/", methods=['POST'])
+def login():
+    return render_template('login.html', email=request.form.get('email'), password=request.form.get('password'))
 
 
 if __name__ == "__main__":
